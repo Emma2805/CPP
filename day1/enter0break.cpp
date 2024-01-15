a73609e659578df2718f2c26a5f1c78064e80553
Emma2805 <94289820+Emma2805@users.noreply.github.com>
2024-01-15 15:45:36 +0530

    Add files via upload

---
+++ day1/enter0break.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sum =0;
+    int a;
+    
+    while( a!= 0)
+    {
+        cin>>a;
+    cout<<a<<endl;
+    sum = sum + a;
+    }
+    cout<< " final sum = "<< sum;
+    
+}
